735796eba5d7414481e7d8c44e852f59b7dcf137
tomstao <96sutao@gmail.com>
2025-03-05 09:15:39 -0500

    AuthenticateUser added

--- src/Banking/Bank.java
+++ src/Banking/Bank.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Bank {
-    private Map<String,User> accounts;
+    private Map<String,User> users;
     static private Bank bank;
 
     private Bank() {
-        this.accounts = new HashMap<>();
+        this.users = new HashMap<>();
     }
 
     static public Bank getInstance() {
@@ -18,10 +18,20 @@ public class Bank {
 
     public void registerUser() {
         User newUser = new User();
-        accounts.put(newUser.getAccountNumber(), newUser);
+        users.put(newUser.getAccountNumber(), newUser);
         System.out.println("New user registered: " + newUser.getAccountNumber());
     }
 
+    public User authenticateUser(String accountNumber, String password) {
+        User user = users.get(accountNumber);
+        if (user != null && user.verifyPassword(password)) {
+            System.out.println("Login successful! Welcome " + user.getFirstName());
+            return user;
+        } else {
+            System.out.println("Invalid account number or password.");
+            return null;
+        }
+    }
 
 
 
